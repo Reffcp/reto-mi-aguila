fe766522161432a886e4fb38c17eabea81d2ee81
Cristian Refugio Covarrubias Patiño <cristian.covarrubias@rebits.com.mx>
2020-09-15 18:02:47 -0500

    terminado estilo general de la barra de navegación

--- src/app/layout/components/sidebar/sidebar.component.ts
+++ src/app/layout/components/sidebar/sidebar.component.ts
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
+  menuElements=[
+    {
+      icon:'drive_eta',
+      text:'Pedir Águila',
+      link:'/',
+    },
+    {
+      icon:'place',
+      text:'Vuelos',
+      link:'/',
+    },
+    {
+      icon:'directions',
+      text:'Rutas',
+      link:'/',
+    },
+    {
+      icon:'admin_panel_settings',
+      text:'Administrar',
+      link:'/',
+    },
+    {
+      icon:'analytics',
+      text:'Estadísticas',
+      link:'/',
+    },
+    {
+      icon:'payment',
+      text:'Medios de Pago',
+      link:'/',
+    },
+    {
+      icon:'login',
+      text:'Cerrar sesión',
+      link:'/',
+    },
+  ]
   constructor() { }
-
   ngOnInit(): void {
+    
   }
 
 }
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MaterialModule } from '../share/material/material.module';
 import { LayoutComponent } from './layout.component';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MaterialModule } from '../share/material/material.module';
 
 
 
@@ -11,8 +11,8 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
   declarations: [LayoutComponent,SidebarComponent],
   imports: [
     CommonModule,
-    MaterialModule,
-    LayoutRoutingModule
+    LayoutRoutingModule,
+    MaterialModule
   ]
 })
 export class LayoutModule { }
